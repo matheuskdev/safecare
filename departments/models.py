5397283df8f230dae680796567c7e78c9ad2cdb3
Matheus Guilherme <matheuskdev@gmail.com>
2024-10-15 16:36:45 -0300

    feat: create models for departments

--- departments/models.py
+++ departments/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+from utils import mixins
+
+
+class Department(
+    mixins.TimestampModelMixin,
+    mixins.OwnerModelMixin,
+    mixins.SoftDeleteModelMixin,
+):
+    """
+    Model representing a Department
+    """
+
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+        help_text="Nome do departamento. Deve ser único.",
+    )
+    description = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        help_text="Descrição do departamento. Opcional.",
+    )
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Departamento"
+        verbose_name_plural = "Departamentos"
+        indexes = [
+            models.Index(fields=["name"]),
+        ]
+        constraints = [
+            models.UniqueConstraint(fields=["name"], name="unique_department_name"),
+        ]
+
+    def __str__(self) -> str:
+        return self.name

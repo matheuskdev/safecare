c2a85cc3d47f96fd83b99ace358be84879ec7e71
Matheus Guilherme <matheuskdev@gmail.com>
2024-10-15 16:45:16 -0300

    feat: create models for accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,87 @@
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+from departments.models import Department
+from utils import regex
+
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("O campo de email deve ser preenchido.")
+
+        email = self.normalize_email(email)
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(unique=True)
+    username = models.CharField(
+        max_length=18,
+        unique=True,
+        validators=[
+            MinLengthValidator(
+                limit_value=4,
+                message="O nome de usuário deve ter no mínimo 4 caracteres.",
+            )
+        ],
+    )
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    bio = models.TextField(blank=True, null=True, max_length=1012)
+    website = models.URLField(blank=True, null=True)
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures/", blank=True, null=True
+    )
+    phone = models.CharField(
+        max_length=20,
+        validators=[regex.phone_regex],
+        help_text="Número de telefone/celular",
+        null=True,
+        blank=True,
+    )
+
+    departments = models.ManyToManyField(Department, related_name="users", blank=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    last_login = models.DateTimeField(null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    class Meta:
+        ordering = ["email"]
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+        indexes = [
+            models.Index(fields=["email"]),
+        ]
+        constraints = [
+            models.UniqueConstraint(fields=["email"], name="unique_user_email"),
+            models.UniqueConstraint(fields=["username"], name="unique_user_username"),
+        ]
+
+    def __str__(self):
+        return self.email

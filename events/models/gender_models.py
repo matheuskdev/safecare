552366529fb9c8cdeb6948cb265770769aeea311
lelylp <weslley3442@hotmail.com>
2024-11-05 14:03:00 -0300

    fix: rename modules

--- events/models/gender_models.py
+++ events/models/gender_models.py
@@ -1,16 +1,16 @@
-"""Models for genere."""
+"""Models for gender."""
 from django.db import models
 
 
-class Genere(models.Model):
-    """Class for genere"""
+class Gender(models.Model):
+    """Class for gender"""
     name = models.TextField(
         max_length=255,
         help_text='Gênero'
     )
 
     class Meta:  # pylint: disable=too-few-public-methods
-        """Class Meta for genere."""
+        """Class Meta for gender."""
 
         ordering = ['name']
         verbose_name = 'Gênero'

5f957337380985b624353a2bcd9a3d4bfe432ad0
lelylp <weslley3442@hotmail.com>
2024-11-06 12:56:58 -0300

    docs: create docstring for "events/services/."

--- events/services/response_ocurrence_services.py
+++ events/services/response_ocurrence_services.py
@@ -6,23 +6,63 @@ from classifications.models import (
 
 
 class CalculateDeadline:
-    """Calculate Deadline"""
+    """
+    Service class to calculate the deadline for response to an occurrence.
+
+    This class calculates the response deadline based on the classifications of
+    the occurrence and the damage. The deadline is determined by summing a number
+    of days based on these classifications.
+
+    Attributes:
+        ocurrence_classification (OcurrenceClassification): The classification of the occurrence.
+        damage_classification (DamageClassification): The classification of the damage.
+
+    Methods:
+        __init__(ocurrence, damage):
+            Initializes the CalculateDeadline instance with the given occurrence and damage classifications.
+
+        calculate():
+            Calculates and returns the deadline for response based on the occurrence and damage classifications.
+    """
+
     def __init__(
             self,
             ocurrence: OcurrenceClassification,
             damage: DamageClassification
     ) -> None:
+        """
+        Initializes the CalculateDeadline instance with the provided occurrence and damage classifications.
+
+        Args:
+            ocurrence (OcurrenceClassification): The occurrence classification object.
+            damage (DamageClassification): The damage classification object.
+        """
         self.ocurrence_classification = ocurrence
         self.damage_classification = damage
 
-    def calculate(self,):
+    def calculate(self):
         """
-        Calcula o prazo de retorno com base nas classificações de ocorrência
-        e dano.
-        O prazo é definido somando-se um número de dias com base nas
-        classificações.
-        """
+        Calculates the deadline for response based on the occurrence and damage classifications.
 
+        The deadline is defined by adding a certain number of days according to the 
+        classifications of the occurrence and the damage.
+
+        Occurrence Classification:
+            - "Improcedente": 1 day
+            - "Não conformidade", "Circustância de Risco", "Quebra de contratualização", 
+              "Desvio da Qualidade": 15 days
+            - "Incidente sem dano": 10 days
+
+        Damage Classification:
+            - "Nenhum": 15 days
+            - "Dano Leve": 7 days
+            - "Dano Moderado": 5 days
+            - "Dano Grave": 3 days
+            - "Dano Óbito": 15 days
+
+        Returns:
+            int: The total number of days for the response deadline.
+        """
         days_of_response = 0
 
         # Classificação da ocorrência
